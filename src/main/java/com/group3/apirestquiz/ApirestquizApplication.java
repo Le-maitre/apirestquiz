c1f3c71787f7227b60520b06f50767cc4992e1ef
Le maitre <kalydiallo741@gmail.com>
2023-07-20 18:14:00 +0000

    First commit

---
+++ src/main/java/com/group3/apirestquiz/ApirestquizApplication.java
@@ -0,0 +1,13 @@
+package com.group3.apirestquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group3/apirestquiz/ApirestquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.apirestquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
